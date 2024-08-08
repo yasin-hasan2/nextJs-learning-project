355d30c23d28e3a777d01fdbdd7787770a2afa0f
yasin al hasan <ya705668@gmail.com>
2024-08-08 15:49:24 +0600

    link navigation in next js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -9,65 +10,10 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
-  const navItems = (
-    <>
-      <li>
-        <a>Home</a>
-      </li>
-      <li>
-        <a>About</a>
-      </li>
-      <li>
-        <a>Submenu 2</a>
-      </li>
-      <li>
-        <a>Item 3</a>
-      </li>
-    </>
-  );
-
   return (
     <html lang="en">
       <body className={inter.className}>
-        <div className="navbar bg-base-100">
-          <div className="navbar-start">
-            <div className="dropdown">
-              <div
-                tabIndex={0}
-                role="button"
-                className="btn btn-ghost lg:hidden"
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-5 w-5"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M4 6h16M4 12h8m-8 6h16"
-                  />
-                </svg>
-              </div>
-              <ul
-                tabIndex={0}
-                className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
-              >
-                {navItems}
-              </ul>
-            </div>
-            <a className="btn btn-ghost text-xl">daisyUI</a>
-          </div>
-          <div className="navbar-center hidden lg:flex">
-            <ul className="menu menu-horizontal px-1">{navItems}</ul>
-          </div>
-          <div className="navbar-end">
-            <a className="btn">Button</a>
-          </div>
-        </div>
+        <Navbar></Navbar>
         {children}
         <footer className="footer bg-neutral text-neutral-content p-10">
           <aside>
