065be8b151c078bc471122cf75acf73fe858bf6b
yasin al hasan <ya705668@gmail.com>
2024-08-11 17:56:03 +0600

    error and loading page setup

---
+++ src/app/error.js
@@ -0,0 +1,12 @@
+"use client";
+import React from "react";
+
+const error = () => {
+  return (
+    <div>
+      <h1> this is error page .....</h1>
+    </div>
+  );
+};
+
+export default error;
---
+++ src/app/loading.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const loading = () => {
+  return (
+    <div className="w-full h-full mx-auto">
+      <span className="loading loading-ring loading-lg"></span>
+    </div>
+  );
+};
+
+export default loading;
